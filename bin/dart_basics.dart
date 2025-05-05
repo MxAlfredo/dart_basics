7f633007989dc5bb0f4a69d6acc7759a115f87f8
Jose Alfredo Escobar Rodriguez <mxjosealfredo@gmail.com>
2025-05-05 11:22:26 -0600

    Implement age calculation in main method and update tests for calculateAge function

--- bin/dart_basics.dart
+++ bin/dart_basics.dart
@@ -1,9 +1,23 @@
+import 'dart:io';
+
 import 'package:dart_basics/dart_basics.dart' as dart_basics;
 
 void main(List<String> arguments) {
-  dart_basics.main();
+  //dart_basics.main();
 
   //Calculate Age
-  int birthYear = 1995;
-  print('Age: ${dart_basics.calculateAge(birthYear)}');
+  print('Enter your birth year:');
+  String birthYearString = stdin.readLineSync() ?? '2000';
+  // Check if the input is a valid integer
+  int birthYear = int.tryParse(birthYearString) ?? 2000;
+  int age = dart_basics.calculateAge(birthYear);
+  print('Your age is: $age');
+
+  if (age < 18) {
+    print('You are a minor.');
+  } else if (age >= 18 && age < 60) {
+    print('You are an adult.');
+  } else {
+    print('You are a senior citizen.');
+  }
 }
